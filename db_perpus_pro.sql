6ca977514ef5ee58541c9bc1c9d085e197aa7f16
Dezaarlian <deza9244@gmail.com>
2025-12-07 13:19:52 +0700

    update db

--- db_perpus_pro.sql
+++ db_perpus_pro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2025 at 10:55 AM
+-- Generation Time: Dec 07, 2025 at 07:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -62,7 +62,9 @@ CREATE TABLE `anggota` (
 INSERT INTO `anggota` (`id_anggota`, `nis`, `nama_siswa`, `kelas`, `jurusan`) VALUES
 (1, '101', 'Budi Santoso', 'XII', 'RPL'),
 (2, '102', 'Siti Aminah', 'XI', 'TKJ'),
-(3, '103', 'Ahmad Dani', 'X', 'Multimedia');
+(3, '103', 'Ahmad Dani', 'X', 'Multimedia'),
+(4, '104', 'Deza Arlian', 'XI', 'TKJ'),
+(5, '105', 'Bima Ardiyansyah', 'XII', 'RPL');
 
 -- --------------------------------------------------------
 
@@ -85,9 +87,10 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `judul`, `jenis_buku`, `pengarang`, `penerbit`, `tahun_terbit`, `rak`) VALUES
-(2, 'Belajar PHP', NULL, 'Rhama Krisna', 'Informatika', 2023, 'Rak B4'),
-(3, 'SI', NULL, 'ANI', 'ANU', 2011, 'B3'),
-(4, 'TI', 'Pelajaran', 'Prisma', 'ert', 2023, 'B3');
+(2, 'Belajar PHP', 'Pelajaran', 'Rhama Krisna', 'Informatika', 2023, 'Rak B4'),
+(3, 'SI', 'Pelajaran', 'ANI', 'ANU', 2011, 'B3'),
+(4, 'TI', 'Pelajaran', 'Prisma', 'ert', 2023, 'B3'),
+(5, 'Cara menghargai wanita', 'Lainnya', 'Ahmad kurnia Prisma', 'Benca Cost', 2025, 'C5');
 
 -- --------------------------------------------------------
 
@@ -105,6 +108,14 @@ CREATE TABLE `transaksi` (
   `denda` int(11) DEFAULT 0,
   `status` enum('pinjam','kembali') DEFAULT 'pinjam'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_buku`, `id_anggota`, `peminjam`, `tgl_pinjam`, `tgl_kembali`, `denda`, `status`) VALUES
+(1, 2, 4, NULL, '2025-11-27', NULL, 0, 'pinjam'),
+(2, 5, 5, NULL, '2025-11-27', '2025-11-27', 0, 'kembali');
 
 --
 -- Indexes for dumped tables
@@ -148,19 +159,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
